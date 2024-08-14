ca95806a0b8ed0e27abca48b59e00238fa45d5da
Sarthak Gupta <113797468+sarthakgupta04@users.noreply.github.com>
2024-08-14 14:29:21 +0530

    Time: 3 ms (62.26%), Space: 53.3 MB (19.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,24 @@
 class Solution {
-  public int majorityElement(int[] nums) {
-    Integer ans = null;
-    int count = 0;
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
 
-    for (final int num : nums) {
-      if (count == 0)
-        ans = num;
-      count += num == ans ? 1 : -1;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
     }
 
-    return ans;
-  }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println("Majority element in nums1: " + solution.majorityElement(nums1));  // Output: 3
+        System.out.println("Majority element in nums2: " + solution.majorityElement(nums2));  // Output: 2
+    }
 }
