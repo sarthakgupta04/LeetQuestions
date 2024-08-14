5a162b2fce6232393c9454071800d9faa2668567
Sarthak Gupta <113797468+sarthakgupta04@users.noreply.github.com>
2024-08-14 14:33:51 +0530

    Time: 12 ms (35.11%), Space: 49.2 MB (84.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        Integer candidate = null;
-
+        Map<Integer, Integer> map = new HashMap<>(); 
+        int n = nums.length / 2; 
         for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            count += (num == candidate) ? 1 : -1;
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        return candidate;
+        int ans = 0; 
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                ans = entry.getKey(); 
+            }
+        }
+        return ans; 
     }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums1 = {3, 2, 3};
         int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
 
-        System.out.println("Majority element in nums1: " + solution.majorityElement(nums1));  // Output: 3
-        System.out.println("Majority element in nums2: " + solution.majorityElement(nums2));  // Output: 2
+        System.out.println("Majority element in nums1: " + solution.majorityElement(nums1));  
+        System.out.println("Majority element in nums2: " + solution.majorityElement(nums2));  
     }
 }
