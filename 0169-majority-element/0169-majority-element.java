0c8daa58e9e2effb6ffb6f591cc278c354c5b9b4
Sarthak Gupta <113797468+sarthakgupta04@users.noreply.github.com>
2024-08-14 14:35:11 +0530

    Time: 2 ms (75.88%), Space: 53.3 MB (19.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,14 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>(); 
-        int n = nums.length / 2; 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        int ans = 0; 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > n) {
-                ans = entry.getKey(); 
-            }
-        }
-        return ans; 
-    }
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums1 = {3, 2, 3};
-        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+  public int majorityElement(int[] nums) {
+    Integer ans = null;
+    int count = 0;
 
-        System.out.println("Majority element in nums1: " + solution.majorityElement(nums1));  
-        System.out.println("Majority element in nums2: " + solution.majorityElement(nums2));  
+    for (final int num : nums) {
+      if (count == 0)
+        ans = num;
+      count += num == ans ? 1 : -1;
     }
+
+    return ans;
+  }
 }
