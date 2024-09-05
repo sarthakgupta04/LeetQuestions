1face67add711ac38dd74d0b833bb4cb79a57672
Sarthak Gupta <113797468+sarthakgupta04@users.noreply.github.com>
2024-09-05 15:13:32 +0530

    Time: 3 ms (98.03%), Space: 62 MB (30.82%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.java
+++ 2028-find-missing-observations/2028-find-missing-observations.java
@@ -1,16 +1,32 @@
 class Solution {
-  public int[] missingRolls(int[] rolls, int mean, int n) {
-    final int targetSum = (rolls.length + n) * mean;
-    int missingSum = targetSum - Arrays.stream(rolls).sum();
-    if (missingSum > n * 6 || missingSum < n)
-      return new int[] {};
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+ int m = rolls.length;
+        int totalSum = mean * (n + m);
+        int observedSum = 0;
+        
+        for (int roll : rolls) {
+            observedSum += roll;
+        }
+        
+        int missingSum = totalSum - observedSum;
+        if (missingSum < n || missingSum > 6 * n) {
+            return new int[0];
+        }
+        
+        int quotient = missingSum / n;
+        int remainder = missingSum % n;
+        
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = quotient;
+        }
+        
+        for (int i = 0; i < remainder; i++) {
+            result[i]++;
+        }
+        
+        return result;
+    }
 
-    int[] ans = new int[n];
-    Arrays.fill(ans, missingSum / n);
-    missingSum %= n;
-    for (int i = 0; i < missingSum; ++i)
-      ++ans[i];
-
-    return ans;
-  }
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();}}
