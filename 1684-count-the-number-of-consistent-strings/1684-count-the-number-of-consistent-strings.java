f3dac3b2047c9a327f375cc035bba72bde0130ae
Sarthak Gupta <113797468+sarthakgupta04@users.noreply.github.com>
2024-09-12 21:31:54 +0530

    Time: 12 ms (63.71%), Space: 45.7 MB (25.22%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -1,7 +1,22 @@
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        return (int) Arrays.stream(words)
-            .filter(word->word.matches(String.format("[%s]*", allowed)))
-            .count();
+        Set<Character> allowedSet=new HashSet<>();
+        for(char c: allowed.toCharArray()){
+            allowedSet.add(c);
+        }
+        int count=0;
+        for(String word:words){
+            boolean isConsistent=true;
+            for(char c: word.toCharArray()){
+                if(!allowedSet.contains(c)){
+                    isConsistent=false;
+                    break;
+                }
+            }
+            if(isConsistent){
+                count++;
+            }
+        }
+        return count;
     }
 }
